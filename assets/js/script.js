b74099c455ac3dcfd6fc65c9413538851a85c336
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-05-16 21:22:55 -0400

    edits from zoom May 16

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 //this fixed it
 var randomDrinks = [];
+var btnOne = $("#btn-one")
+var btnTwo = $("#btn-two")
 
-$("#no-btn").on("click", function () {
+$("#btn-two").on("click", function () {
   $("#question").text("Ha, You're not Bad Enough");
   $("#image").attr("src", "assets/image/bbNo.jpg");
   $("#card-bodytext").text("");
-  $("#yes-btn").css("display", "none");
-  $("#no-btn").css("display", "none");
+  $("#btn-one").css("display", "none");
+  $("#btn-two").css("display", "none");
   $("<button>")
     .addClass("ui inverted red button centered")
     .attr("id", "home")
@@ -18,17 +20,17 @@ $("#no-btn").on("click", function () {
   });
 });
 
-$("#yes-btn").on("click", function () {
+$("#btn-one").on("click", function () {
   $("#question").text("Pick Your Poison");
   $("#image").attr("src", "assets/image/mask.jpg");
   $("#card-bodytext").text("TO SEE HOW BAD YOU ARE...");
-  $("#yes-btn").css("display", "none");
-  $("#no-btn").css("display", "none");
+  $("#btn-one").addClass("ui inverted pink button centered")
+  .attr("id", "dare-btn")
+  .text("Click if You Dare")
+  .appendTo("#button-group");
+  $("#btn-two").css("display", "none");
   $("<button>")
-    .addClass("ui inverted pink button centered")
-    .attr("id", "dare-btn")
-    .text("Click if You Dare")
-    .appendTo("#button-group");
+    
 
   $("#dare-btn").on("click", function () {
     $(".content").css("display", "none");
@@ -60,6 +62,22 @@ $("#yes-btn").on("click", function () {
 
 $(document).on("click", ".drink-btn", function characterSelect() {
   console.log($(this).attr("data-name"));
+  $("#drink-div").css("display", "none")
+  $("<h1>").attr("id", "characterHeader").addClass("ui header centered").text("Character Name").appendTo("#container")
+  $("<div>").addClass("ui segment").attr("id", "character-card").appendTo("#container")
+  $("<img>").addClass("ui small left floated image").attr("src", "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%253AANd9GcQVt3zxl9IrraWLsPVL5cWObwrokplViJmDBHgW-KJbJ8-RyCWH%26usqp%3DCAU").appendTo("#character-card");
+  $("<p>").text("pi Character Info here: Walter White was an only child. Walt's father died of Huntington's disease when he was six years old. He studied chemistry at the California Institute of Technology, where he conducted research on proton radiography that helped a team win a Nobel Prize in Chemistry in 1985").appendTo("#character-card")
+  $("<p>").text("ADrink API: White Russian:Ingredients 1-1/2 ounces vodka, 1-1/2 ounces Kahlua, 3 ounces heavy whipping cream or milk. Directions: Place ice in a rocks glass. Add vodka and Kahlua; top with cream.").appendTo("#character-card")
+  $("<p>").text("Reason why you would be character based off drink selection. ie Walter White lives a simple life but has a desire to take on the responsibility of providing for his family's future. The White Russian is a simple yet complicated drink which matches well with Walter White. ").appendTo("#character-card")
+  $("<img>").addClass("ui small left floated image").attr("src", "https://f1.pngfuel.com/png/905/307/702/retro-pop-art-vintage-white-russian-black-russian-irish-cream-cocktail-garnish-rum-and-coke-png-clip-art.png").appendTo("#character-card");
+  $("<br>").appendTo("#character-card")
+  $("<div>").attr("id", "button-group2").addClass("ui two bottom attached buttons").appendTo("#container")
+  btnOne.css("display", "block").appendTo("#button-group2").text("Try Again")
+  btnTwo.css("display", "block").appendTo("#button-group2").text("Home")
+  
+  
+
+
 });
 // $.ajax({
 //   url: "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic",
