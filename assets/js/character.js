d16336ee0d0b7e07e26a824a747859bb04c3c3a9
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-18 15:12:53 -0400

    removed console logs and added comments to character.js file

--- assets/js/character.js
+++ assets/js/character.js
@@ -4,39 +4,38 @@ var drinkID = "";
 var charInfo = {
     1: "Walter White was an only child. Walt's father died of Huntington's disease when he was six years old. He studied chemistry at the California Institute of Technology, where he conducted research on proton radiography that helped a team win a Nobel Prize in Chemistry in 1985",
     2: "",
+    3: "",
+    5: "",
+    6: "",
+    8: "",
 }
 var reasonInfo = {
     1: "Reason why you would be character based off drink selection. ie Walter White lives a simple life but has a desire to take on the responsibility of providing for his family's future. The White Russian is a simple yet complicated drink which matches well with Walter White.",
     2: "",
+    3: "Skyler White is a White Lady because she is sweet yet sour, similar to the triple sec, lemon juice and gin.",
+    5: "",
+    6: "Marie Schrader is an Aviation because she wears purple throughout the series. She believes it represents royalty, luxury and safety from the meth trade (purple is opposite of yellow).",
+    8: "",
 }
 
 $(document).on("click", ".drink-btn", function characterSelect() {
-
+    // Troubleshoot for correct character and drink IDs
     console.log($(this).attr("data-char"))
-    
-            // Empty variables from any previous use
-            charID = "";
-            drinkID = "";
-    
-            // Pull the data attribute values assigned to the button
-            charID = $(this).attr("data-char");
-            console.log(charID)
-            drinkID = $(this).attr("data-drink");
-            console.log(drinkID)
+    console.log($(this).attr("data-drink"))
+    // Empty variables from any previous use
+    charID = "";
+    drinkID = "";
+    // Pull the data attribute values assigned to the button
+    charID = $(this).attr("data-char");
+    drinkID = $(this).attr("data-drink");
 
-getCharDrinkInfo();
+    getCharDrinkInfo();
 
     // Button listener for the 
-function getCharDrinkInfo() {
-
-        console.log("drink button 1")
+    function getCharDrinkInfo() {
         $("#drink-div").css("display", "none");
-
-
-
         // Pass the variables to their respective functions
         getCharInfo(charID, drinkID);
-
     };
 
     // Create dynamic character information
@@ -46,16 +45,13 @@ function getCharDrinkInfo() {
             url: "https://www.breakingbadapi.com/api/characters/" + charID + "",
             method: "GET",
         }).then(function (response) {
-            console.log(response)
             // Character Name
             var charName = response[0].name;
             $("<h1>").attr("id", "characterHeader").addClass("ui header centered").text("You are " + charName).appendTo("#container");
             $("<div>").addClass("ui segment").attr("id", "character-card").appendTo("#container")
-
             // Character Image
             var charImg = response[0].img;
             $("<img>").addClass("ui small left floated image").attr("src", charImg).appendTo("#character-card");
-
             // Character Info
             var newCharInfo = $("<p>").text(charInfo[charID])
             console.log(newCharInfo)
@@ -107,20 +103,18 @@ function getCharDrinkInfo() {
             if (response.drinks[0].strIngredient9) {
                 drinkIngredient.push(response.drinks[0].strIngredient9)
             };
-            console.log(drinkIngredient);
 
             // Need to set this to an unordered list for drink ingredients
             $("<h3>").addClass("ui header").text("Recipe:").appendTo("#character-card");
             $("<ul>").addClass("ui list").attr("id", "ingList2").appendTo("#character-card");
 
+            // Loop to create list item for each existing ingredient
             for (i = 0; i < drinkIngredient.length; i++) {
                 $("<li>").text(drinkIngredient[i]).appendTo("#ingList2")
             }
 
             // Drink Instructions
-            console.log(response.drinks[0].strInstructions);
             $("<div>").addClass("description").text(response.drinks[0].strInstructions).appendTo("#character-card");
-
 
             // Reason character and drink match
             $("<p>").text(reasonInfo[charID]).appendTo("#character-card");
@@ -133,9 +127,6 @@ function getCharDrinkInfo() {
             $("#home").on("click", function () {
                 location.reload();
             });
-
         });
     };
-
 });
-
