1c8c2dd88186d34697bc6b0f6285abb328bd086c
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-05-19 00:19:37 -0400

    created random quotes page, deleted game js/html & worked on styling

---
+++ assets/js/quote.js
@@ -0,0 +1,38 @@
+$("#menu-quote").on("click", function () {
+  console.log("Bad Quote");
+  bbBadQuotes();
+});
+
+var quoteID = ""
+function bbBadQuotes() {
+  $(".content").css("display", "none");
+  $("#pop-up").css("display", "none");
+  $("#image").css("display", "none");
+  $("#dare-btn").css("display", "none");
+
+
+
+    $.ajax({
+        url: "https://www.breakingbadapi.com/api/quote/random" + quoteID + "",
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        $("<div>")
+        .addClass("ui centered card")
+        .attr("id", "ui-card2")
+        .appendTo("#container");
+      $("<div>").addClass("image").appendTo("#ui-card2");
+      $("<img>")
+        .addClass("ui image")
+        .attr("src", "https://i.pinimg.com/originals/19/83/c0/1983c001dcc14ed4636130e0fea8420f.jpg")
+        .appendTo("#ui-card2");
+      $("<div>").addClass("content").appendTo("ui-card2");
+      $("<h3>").addClass("ui header").attr("id", "random-quote").text(response[0].quote).appendTo("#ui-card2");
+      $("<div>")
+      .addClass("ui centered")
+      .attr("id", "quote-author")
+      .text("- " + response[0].author)
+      .appendTo("#ui-card2");
+
+});
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -156,21 +156,27 @@ function pickYourPoison() {
   $("#question").text("Pick Your Poison");
   $("#image").attr("src", "assets/image/mask.jpg");
   $("#card-bodytext").text("TO SEE HOW BAD YOU ARE...");
+  $("<div>").addClass("ui buttons").attr("id", "btn-group")
   $("#btn-one")
     .addClass("ui inverted pink button centered")
     .attr("id", "dare-btn")
-    .text("Click if You Dare")
+    .text("Drink if You Dare")
     .appendTo("#button-group");
   $("#btn-two").addClass("ui inverted violet button centered")
     .attr("id", "lucky")
     .text("Feelin' lucky?")
     .appendTo("#button-group");
+   
 
 
 
   $("#lucky").on("click", function () {
     feelingLucky();
   });
+
+  $("#quoteBTN").on("click", function(){
+    bbBadQuotes()
+  })
 
   $("#dare-btn").on("click", function () {
     $(".content").css("display", "none");
@@ -241,6 +247,8 @@ function createDrinkTiles(newCharID, newDrinkID) {
       .appendTo("#drink-div2");
   });
 };
+
+
 
 $("#menu-pyp").on("click", function () {
   console.log("sad panda");
